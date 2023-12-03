108b1a8f7c4ce43c759900027b3824a511af167b
ilvlsq <evgeny.kucherenko004@gmail.com>
2023-12-03 11:43:05 +0100

    fix

--- src/app/api/contact-book/contactsActions.ts
+++ src/app/api/contact-book/contactsActions.ts
@@ -2,8 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { options } from "../auth/[...nextauth]/options";
 
+const prisma = new PrismaClient();
+
 export async function getContacts() {
-  const prisma = new PrismaClient();
   const contacts = await prisma.phoneBookContact.findMany();
   const session = await getServerSession(options);
   const contactsOfCurrentUser = contacts.filter(
